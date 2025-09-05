9add63ce6f02fa327f43f226db3e39f01b05c115
Aleksandr Zarubin <zarubin_14@list.ru>
2025-09-05 13:14:52 +0300

    fix(products): fix create path

--- app/routers/product.py
+++ app/routers/product.py
@@ -110,7 +110,7 @@ def get_product_by_sku(sku: str, db: Session = Depends(get_db)):
 
 
 @router.post(
-    "/",
+    "",
     response_model=ProductOut,
     dependencies=[Depends(require_admin_role)],
 )
